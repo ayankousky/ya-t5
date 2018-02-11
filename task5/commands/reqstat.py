fa09a3555c07b36d25126bcf86163f4a574e76ab
Alex Yankousky <ayankousky@yahoo.com>
2018-02-11 23:33:08 +0300

    ADD: reqstat command

--- task5/commands/reqstat.py
+++ task5/commands/reqstat.py
@@ -1,14 +1,30 @@
 """The hello command."""
 
-
 from json import dumps
 
 from .base import Base
+import csv
+import operator
+
+
+class Users: pass
 
 
 class ReqStat(Base):
-    """Say hello, world!"""
+	"""Say hello, world!"""
 
-    def run(self):
-        print('kekeke')
-        # print('You supplied the following options:', dumps(self.options, indent=2, sort_keys=True))
+	def run(self):
+		users = {}# Users();
+		# with open('/Users/alex/Desktop/short.csv', 'rb') as csvfile:
+		with open('/Users/alex/Desktop/shkib.csv', 'rb') as csvfile:
+			journal = csv.reader(csvfile, delimiter=',', quotechar='|')
+			for row in journal:
+				if row[1] == '""src_user""':
+					continue
+				if users.get(row[1]):
+					users[row[1]] += 1
+				else:
+					users[row[1]] = 1
+			top_req_users = sorted(users.items(), key=operator.itemgetter(1), reverse=True)
+			for user in top_req_users[0:6]:
+				print user[0] +' '+str(users.get(user[0]))
